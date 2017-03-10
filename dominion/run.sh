354dab1ac4c943c5b48be376710c57999d43426a
Evgeny Golyshev <eugulixes@gmail.com>
2017-03-10 21:07:50 +0300

    Uncomment run.sh

--- dominion/run.sh
+++ dominion/run.sh
@@ -1,9 +1,7 @@
-apt-get update
+cd ${RPI2_BUILDER_LOCATION}
+./rpi23-gen-image.sh
 
-# cd ${RPI2_BUILDER_LOCATION}
-# ./rpi23-gen-image.sh
-# 
-# cd ${WORKSPACE_DIR}
-# tar czf ${BUILD_ID}.tar.gz ${BUILD_ID}.img ${BUILD_ID}.bmap
-# rm ${BUILD_ID}.bmap
-# rm ${BUILD_ID}.img
+cd ${WORKSPACE_DIR}
+tar czf ${BUILD_ID}.tar.gz ${BUILD_ID}.img ${BUILD_ID}.bmap
+rm ${BUILD_ID}.bmap
+rm ${BUILD_ID}.img
