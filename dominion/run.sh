33c80e336776cb0e43179ced10d22c2d99b3c887
Evgeny Golyshev <eugulixes@gmail.com>
2017-12-05 20:46:40 +0300

    Put built images to tar.gz

--- dominion/run.sh
+++ dominion/run.sh
@@ -5,5 +5,7 @@ set -e
 cd ${PIEMAN_PATH}
 ./pieman.sh
 mv build/${PROJECT_NAME}/${PROJECT_NAME}.img ${WORKSPACE}
-gzip ${WORKSPACE}/${PROJECT_NAME}.img
 rm -r build/${PROJECT_NAME}
+cd ${WORKSPACE}
+tar czf ${PROJECT_NAME}.tar.gz ${PROJECT_NAME}.img
+rm ${PROJECT_NAME}.img
