dd2ba5d94e51e69fcdb8d92bdb9810fad36abe79
Denis Mosolov <denismosolov@gmail.com>
2018-03-17 17:53:02 +0300

    Replace tar.gz with img.gz

--- dominion/run.sh
+++ dominion/run.sh
@@ -4,8 +4,5 @@ set -e
 
 cd ${PIEMAN_PATH}
 ./pieman.sh
-mv build/${PROJECT_NAME}/${PROJECT_NAME}.img ${WORKSPACE}
+mv build/${PROJECT_NAME}/${PROJECT_NAME}.img.gz ${WORKSPACE}
 rm -r build/${PROJECT_NAME}
-cd ${WORKSPACE}
-tar czf ${PROJECT_NAME}.tar.gz ${PROJECT_NAME}.img
-rm ${PROJECT_NAME}.img
