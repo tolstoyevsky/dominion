c6f712e1ceeea60ce5e32a11d827d42937e0787e
Danil <mdanil007@mail.ru>
2018-11-27 11:44:27 +0300

    Add mender params

--- dominion/run.sh
+++ dominion/run.sh
@@ -4,4 +4,13 @@ set -e
 
 cd ${PIEMAN_PATH}
 ./pieman.sh
-mv build/${PROJECT_NAME}.img.gz ${WORKSPACE}
+
+if [ -z ${CREATE_ONLY_MENDER_ARTIFACT+x} ]; then
+    CREATE_ONLY_MENDER_ARTIFACT=false
+fi
+
+if ${CREATE_ONLY_MENDER_ARTIFACT}; then
+    mv build/${PROJECT_NAME}.mender ${WORKSPACE}
+else
+    mv build/${PROJECT_NAME}.img.gz ${WORKSPACE}
+fi
