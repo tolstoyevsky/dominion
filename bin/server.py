d6da3b55ed76aff1888ed5ef1f14f6f517513454
Evgeny Golyshev <eugulixes@gmail.com>
2020-10-29 20:01:10 +0300

    Read build logs from Redis channel instead of text file

--- bin/server.py
+++ bin/server.py
@@ -13,21 +13,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-import pty
-import signal
-
+import aioredis
 import tornado.options
 import tornado.web
 import tornado.websocket
 from shirow.ioloop import IOLoop
 from shirow.server import RPCServer, TOKEN_PATTERN, remote
-from tornado.options import define, options
+from tornado.options import options
 
-define('build_log_dir', help='The path to the directory which contains build logs',
-       default='/tmp/dominion')
-
-BUF_SIZE = 65536
+from dominion.settings import REDIS_HOST, REDIS_PORT
 
 
 class Application(tornado.web.Application):
@@ -42,35 +36,22 @@ class Dominion(RPCServer):
     def __init__(self, application, request, **kwargs):
         super().__init__(application, request, **kwargs)
 
-        self._fd = None
-        self._pid = None
+        self._subscriber = None
 
     def destroy(self):
-        if self._fd:
-            self.io_loop.remove_handler(self._fd)
-
-        if self._pid:
-            os.kill(self._pid, signal.SIGKILL)  # kill zombie process
+        if self._subscriber:
+            self._subscriber.close()
 
     @remote
-    async def get_rt_build_log(self, request, build_id):
-        self._pid, self._fd = pty.fork()
-        if self._pid == 0:  # child
-            command_line = ['echo', 'Hello, World!', ]
-            os.execvp(command_line[0], command_line)
-        else:  # parent
-            def build_log_handler(*_args, **_kwargs):
-                try:
-                    data = os.read(self._fd, BUF_SIZE)
-                except OSError:
-                    return
+    async def get_rt_build_log(self, request, image_id):
+        self._subscriber = await aioredis.create_redis((REDIS_HOST, int(REDIS_PORT)))
+        res = await self._subscriber.subscribe(f'build-log-{image_id}')
+        channel = res[0]
 
-                request.ret_and_continue(data.decode('utf8'))
-
-            self.io_loop.add_handler(self._fd, build_log_handler, self.io_loop.READ)
-
-            # The parent process finishes not waiting for the child.
-            # The child process becomes a zombie.
+        request.ret_and_continue('The image will start being built in a few seconds...\r\n')
+        while await channel.wait_message():
+            msg = await channel.get()
+            request.ret_and_continue(msg.decode('utf8'))
 
 
 def main():
